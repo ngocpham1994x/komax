37309a9e69ecc641ec043634ef77122512281d3b
ngocpham1994x <ngoc.pham.nm@gmail.com>
2024-07-25 15:51:32 -0500

    finished core function

--- komax/Form1.Designer.cs
+++ komax/Form1.Designer.cs
@@ -28,13 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 128);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(368, 340);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(676, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(368, 340);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(555, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save As";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(676, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(368, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(495, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Zeta to Omega";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(969, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "EXIT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(36, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Convert";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 503);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- komax/Form1.cs
+++ komax/Form1.cs
@@ -3,18 +3,175 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace komax
 {
     public partial class Form1 : Form
     {
+        string zetaDDS = "";
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // Browse button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "DDS Files (*.dds) | *.dds| All Files (*.*)| *.*";
+
+            // If the user selects a file and clicks "OK"
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                zetaDDS = dialog.FileName;
+                textBox1.Text = dialog.FileName;
+
+                string fileContent = ReadFile(zetaDDS);
+                textBox2.Text = fileContent;
+            }
+        }
+
+        private string ReadFile(string path)
+        {
+            try
+            {
+                return File.ReadAllText(path);
+
+                //string[] lines = File.ReadAllLines(path);
+                //richTextBox1.Clear();
+
+                //foreach (string line in lines)
+                //{
+                //    if (line.Contains("TubeMarking="))
+                //    {
+                //        int start = richTextBox1.TextLength;
+                //        richTextBox1.AppendText(line + Environment.NewLine);
+                //        int end = richTextBox1.TextLength;
+
+                //        richTextBox1.Select(start, end - start);
+                //        richTextBox1.SelectionBackColor = System.Drawing.Color.Yellow;
+                //    }
+                //    else
+                //    {
+                //        richTextBox1.AppendText(line + Environment.NewLine);
+                //    }
+                //}
+
+                //richTextBox1.Select(0, 0); // Reset selection
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading file: {ex.Message}");
+                return string.Empty;
+            }
+        }
+        
+        private void ZetaToOmega(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("TubeMarking="))
+                {
+                    string[] parts = line.Split(',');
+
+                    // Extract the relevant parts
+                    string tubeMarking = parts[0].Split('=')[1];
+                    string tubeMarkingLayout = parts[1].Trim('"');
+                    string tubeMarkingText1 = parts[2].Trim('"');
+                    string tubeMarkingText2 = parts[4].Trim('"');
+                    string tubeLength = parts[6].Trim('"');
+
+                    string output = 
+                        $@"        [NewTubeMarkingEnd{tubeMarking}]
+                        TubeMarkingLayout={tubeMarkingLayout}
+                        TubeLength={tubeLength}
+                        TubeMarkingText1=""{tubeMarkingText1}""
+                        TubeMarkingText2=""{tubeMarkingText2}""
+                        TubeMarkingTextSize=1.5";
+
+                    textBox3.AppendText(output + Environment.NewLine);
+                }
+                else
+                {
+                    textBox3.AppendText(line+ Environment.NewLine);
+                }
+            }
+
+        }
+
+        // Save As button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            // If the user selects a file and clicks "OK"
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = dialog.SelectedPath;
+            }
+        }
+
+        // EXIT button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure to exit the application?", "Exit Application", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        // Export button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // First check that the user chose a file path
+            if (string.IsNullOrEmpty(textBox4.Text))
+            {
+                MessageBox.Show($"User did not choose a valid file path. Did not export to file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create the full file path for the exported file
+            string exportFilePath = Path.Combine(textBox4.Text, "Article.dds");
+            SaveTextBoxContentToDDS(exportFilePath, textBox3.Text);
+
+            MessageBox.Show($"Data Exported: \n {exportFilePath}", "Result", MessageBoxButtons.OK);
+        }
+
+        public static void SaveTextBoxContentToDDS(string filePath, string content)
+        {
+            try
+            {
+                // Save the content to a DDS file (plain text with .dds extension)
+                File.WriteAllText(filePath, content);
+
+                MessageBox.Show("Content saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving content: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Convert button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ZetaToOmega(zetaDDS);
+        }
     }
+
+
 }
+
+
